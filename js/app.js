97156450bac73aa9f46bf81db62024112d9031d8
Bob Rudis <bob@rud.is>
2017-04-25 15:21:55 -0400

    working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
 var unknown, known, vulndb, cve;
+var unknown_extract, known_extract, vulndb_extract, cve_extract;
+
 var sel_back = "#bcbddc";
 var curr_sel = "cve";
 
@@ -60,9 +62,138 @@ var width=800, height=600;
 var div = d3.select("#vis");
 var svg = div.append("svg").attr("width", width).attr("height", height);
 var scale_fill_score = d3.scaleOrdinal()
-                         .domain(['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10'])
-                         .range(["#00c402", "#00e021", "#00f003", "#d0ff04", "#ffe003", "#ffcc03", "#ffbc12", "#ff9c21", "#ff8002", "#ff0201"]);
+                         .domain(['0-1', '1-2', '2-3', '3-4', '4-5',
+                                  '5-6', '6-7', '7-8', '8-9', '9-10'])
+                         .range(["#00c402", "#00e021", "#00f003", "#d0ff04", "#ffe003",
+                                 "#ffcc03", "#ffbc12", "#ff9c21", "#ff8002", "#ff0201"]);
 
+
+var bars_width = 195, bars_height = 80;
+
+var x = d3.scaleBand().range([0, bars_width]).padding(0.1);
+var y = d3.scaleLinear().range([bars_height, 0]);
+
+var f_svg = d3.select("#f_svg").append("g").attr("transform", "translate(1,1)");
+var o_svg = d3.select("#o_svg").append("g").attr("transform", "translate(1,1)");
+var m_svg = d3.select("#m_svg").append("g").attr("transform", "translate(1,1)");
+var i_svg = d3.select("#i_svg").append("g").attr("transform", "translate(1,1)");
+var e_svg = d3.select("#e_svg").append("g").attr("transform", "translate(1,1)");
+
+function updateBars() {
+
+  var dat;
+
+  if (curr_sel == "cve") dat = cve_extract;
+  if (curr_sel == "vulndb") dat = vulndb_extract;
+  if (curr_sel == "known") dat = known_extract;
+  if (curr_sel == "unknown") dat = unknown_extract;
+
+  var x1 = x.domain(['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10']);
+  var y1 = y.domain([0, dat.max_val]);
+
+  var ent_df = dat.enterprise;
+  var for_df = dat.foreign;
+  var oss_df = dat.oss;
+  var ics_df = dat.ics;
+  var med_df = dat.medical;
+
+//  if (just_crit.property("checked")) {
+//    ent_df = ent_df.filter(d => d.score == '9-10');
+//    for_df = for_df.filter(d => d.score == '9-10');
+//    oss_df = oss_df.filter(d => d.score == '9-10');
+//    ics_df = ics_df.filter(d => d.score == '9-10');
+//    med_df = med_df.filter(d => d.score == '9-10');
+//    x1 = x.domain(['9-10']);
+//  }
+
+  var e1 = e_svg.selectAll(".bar").data(ent_df);
+  var f1 = f_svg.selectAll(".bar").data(for_df);
+  var o1 = o_svg.selectAll(".bar").data(oss_df);
+  var i1 = i_svg.selectAll(".bar").data(ics_df);
+  var m1 = m_svg.selectAll(".bar").data(med_df);
+
+  e1.attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  e1.enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  f1.attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  f1.enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d.score))
+    .attr("width", x.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  o1.attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  o1.enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  m1.attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  m1.enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  i1.attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+  i1.enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x1(d.score))
+    .attr("width", x1.bandwidth())
+    .attr("y", d => y1(d.n))
+    .attr("fill", d => scale_fill_score(d.score))
+    .attr("height", d => (height - y1(d.n)));
+
+//  e1.remove();
+//  f1.remove();
+//  o1.remove();
+//  m1.remove();
+//  i1.remove();
+
+}
 
 var node_g = svg.append("g").attr("class", "nodes");
 var t = d3.transition().duration(100);
@@ -124,16 +255,23 @@ function updateSim(vuln_nodes) {
 
     simulation.restart();
 
+    updateBars();
+
   });
 
 }
 
-function startSim(error, unknown_d, known_d, vulndb_d, cve_d) {
+function startSim(error, unknown_d, known_d, vulndb_d, cve_d, u_e, k_e, v_e, c_e) {
 
   unknown = unknown_d;
   known = known_d;
   vulndb = vulndb_d,
   cve = cve_d;
+
+  unknown_extract = u_e;
+  known_extract = k_e;
+  vulndb_extract = v_e;
+  cve_extract = c_e;
 
   cve_box.style("background", sel_back);
 
@@ -146,5 +284,9 @@ d3.queue()
   .defer(d3.json, "json/all.json")
   .defer(d3.json, "json/vulndb.json")
   .defer(d3.json, "json/cve.json")
+  .defer(d3.json, "json/unknown_extract.json")
+  .defer(d3.json, "json/all_extract.json")
+  .defer(d3.json, "json/vulndb_extract.json")
+  .defer(d3.json, "json/cve_extract.json")
   .await(startSim);
 
--- prep.R
+++ prep.R
@@ -1,5 +1,7 @@
 library(rvest)
+library(broom)
 library(stringi)
+library(jsonlite)
 library(tidyverse)
 
 pg <- read_html("https://www.cvedetails.com/cvss-score-distribution.php")
@@ -10,47 +12,69 @@ html_nodes(pg, "table.grid") %>%
   slice(1:10) %>%
   mutate(pct = as.numeric(percentage)/100) -> cvss_df
 
-set.seed(1492)
-sample(cvss_df$cvss_score, 100, replace=TRUE, prob=cvss_df$pct) %>%
-  sprintf("{ score:'%s', v:'visible', index }", .) %>%
-  paste0(collapse=",") %>%
-  sprintf("[ %s ]", .)
+make_parts <- function(vec, nam) {
 
-n <- 1000
+  all_scores <- c("0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", "9-10")
+
+  set.seed(1492)
+  foreign <- sample(vec, length(vec) * 0.01)
+  foss <- sample(vec, length(vec)*0.10)
+  medical <- sample(vec, length(vec)*0.05)
+  ics <- sample(vec, length(vec)*0.10)
+  enterprise <- sample(vec, length(vec)*0.65)
+
+  foreign <- table(foreign) %>% tidy() %>% rename(score=foreign, n=Freq) %>%
+      mutate(score = as.character(score)) %>% complete(score=all_scores, fill=list(n=0))
+
+  oss <- table(foss) %>% tidy() %>% rename(score=foss, n=Freq) %>%
+      mutate(score = as.character(score)) %>% complete(score=all_scores, fill=list(n=0))
+
+  medical <- table(medical) %>% tidy() %>% rename(score=medical, n=Freq) %>%
+      mutate(score = as.character(score)) %>% complete(score=all_scores, fill=list(n=0))
+
+  ics <- table(ics) %>% tidy() %>% rename(score=ics, n=Freq) %>%
+      mutate(score = as.character(score)) %>% complete(score=all_scores, fill=list(n=0))
+
+  enterprise <- table(enterprise) %>% tidy() %>% rename(score=enterprise, n=Freq) %>%
+      mutate(score = as.character(score)) %>% complete(score=all_scores, fill=list(n=0))
+
+  max_val <- max(c(foreign$n, oss$n, medical$n, ics$n, enterprise$n))
+
+  list(
+    foreign=foreign, oss=oss, medical=medical, ics=ics, enterprise=enterprise, max_val=max_val
+  ) %>% toJSON() %>% cat(sep="", file=sprintf("json/%s_extract.json", nam))
+
+}
 
 set.seed(1492)
-sample(cvss_df$cvss_score, n*3, replace=TRUE, prob=cvss_df$pct) %>%
-  sort() %>%
+unknown <- sample(cvss_df$cvss_score, n*3, replace=TRUE, prob=cvss_df$pct)
+sort(unknown) %>%
   sprintf('{ "s":"%s" }', .) %>%
   paste0(collapse=", ") %>%
   sprintf("[ %s ]", .) %>% write_lines("json/unknown.json")
+make_parts(unknown, "unknown")
 
 set.seed(1492)
-sample(cvss_df$cvss_score, n, replace=TRUE, prob=cvss_df$pct) %>%
-  sort() %>%
+known <- sample(cvss_df$cvss_score, n, replace=TRUE, prob=cvss_df$pct)
+sort(known) %>%
   sprintf('{ "s":"%s" }', .) %>%
   paste0(collapse=", ") %>%
   sprintf("[ %s ]", .) %>% write_lines("json/all.json")
+make_parts(known, "all")
 
 set.seed(1492)
-sample(cvss_df$cvss_score, n*0.6, replace=TRUE, prob=cvss_df$pct) %>%
-  sort() %>%
+vulndb <- sample(cvss_df$cvss_score, n*0.6, replace=TRUE, prob=cvss_df$pct)
+sort(vulndb) %>%
   sprintf('{ "s":"%s" }', .) %>%
   paste0(collapse=", ") %>%
   sprintf("[ %s ]", .) %>% write_lines("json/vulndb.json")
+make_parts(vulndb, "vulndb")
 
 set.seed(1492)
-sample(cvss_df$cvss_score, n*0.2, replace=TRUE, prob=cvss_df$pct) %>%
-  sort() %>%
+cve <- sample(cvss_df$cvss_score, n*0.2, replace=TRUE, prob=cvss_df$pct)
+sort(cve) %>%
   sprintf('{ "s":"%s" }', .) %>%
   paste0(collapse=", ") %>%
   sprintf("[ %s ]", .) %>% write_lines("json/cve.json")
+make_parts(cve, "cve")
 
-
-
-# All: 1000
-# Vuln: 80%
-# CVE: 60%
-100 = 0.6 * x1
-
-600/3
